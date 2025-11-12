5d640fdf57117643a6376582418a543476360298
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-12 15:24:21 +0530

    Added Contact controller

--- backend/admin-backend/src/controllers/Contact.js
+++ backend/admin-backend/src/controllers/Contact.js
@@ -1,5 +1,6 @@
-import z, { success } from "zod";
+import z from "zod";
 import { Seller } from "../models/Seller.js";
+import { Contact } from "../models/Contact";
 
 const contactValidator = z.object({
     firstName: z.string().min(1, "First name is required!"),
@@ -8,7 +9,7 @@ const contactValidator = z.object({
     message: z.string().min(1, "Message is required!")
 });
 
-async function contact(req, res){
+async function createMessage(req, res){
 
     try{
         const parsedResult = contactValidator.safeParse(req.body);
@@ -59,6 +60,61 @@ async function contact(req, res){
     };
 };
 
+async function getAllMessages(req, res){
+
+    try{
+
+        const messages = await Contact.find({}).populate("seller", "firstName lastName email message").sort({
+            createdAt: -1
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "ALl messages fetched successfully!",
+            messages
+        });
+    } catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error
+        });
+    };
+};
+
+async function updateMessages(req, res){
+
+    try{
+        const messageId = req.params;
+        const { status } = req.body;
+
+        const updatedMessage = await Contact.findByIdAndUpdate(
+            messageId,
+            {
+                status
+            },
+            {
+                new: true
+            }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Message updated successfully!",
+            updatedMessage
+        })
+    } catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error
+        });
+    };
+
+}
+
 export {
-    contact
+    createMessage,
+    getAllMessages,
+    updateMessages
 }
