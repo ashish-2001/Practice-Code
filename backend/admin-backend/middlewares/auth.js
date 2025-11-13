45ba4c98669e7a2297530d12ec8783fc5b161ef7
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-13 17:36:10 +0530

    Added auth middleware

---
+++ backend/admin-backend/middlewares/auth.js
@@ -0,0 +1,101 @@
+import { jwt } from "jsonwebtoken";
+
+async function auth(req, res, next){
+
+    const token = req.cookies.token || req.header("Authorization")?.replace("Bearer ", "") || req.body.token;
+
+    if(!token){
+        return res.status(403).json({
+            success: false,
+            message: "Token missing!"
+        });
+    };
+
+    try{
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+        console.log("Decoded:", decode);
+
+        req.user = decode;
+
+        next();
+    } catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error!", 
+            error
+        });
+    };
+}
+
+async function isAdmin(req, res, next){
+
+    try{
+        
+        if(req.user.role !== "Admin"){
+            return res.status(403).json({
+                success: false,
+                message: "This is a protected route for Admin only"
+            });
+        };
+
+        next();
+        
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error
+        });
+    };
+
+}
+
+async function isSeller(req, res, next){
+
+    try{
+        if(req.user.role !== "Seller"){
+            return res.status(403).json({
+                success: false,
+                message: "This is a protected route for Seller only!"
+            });
+        };
+
+        next();
+
+    } catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error
+        });
+    };
+}
+
+async function isCustomer(req, res, next){
+    
+    try{
+        if(req.user.role !== "Customer"){
+            return res.status(403).json({
+                success: false,
+                message: "This is a protected route for Customer only!"
+            });
+        };
+
+        next();
+
+    } catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error
+        });
+    };
+}
+
+export {
+    auth,
+    isAdmin,
+    isCustomer,
+    isSeller
+}
