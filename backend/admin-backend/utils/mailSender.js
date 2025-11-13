7e10fa4def2abbd136b72d964288920889cdb1d2
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-13 18:14:01 +0530

    Added mail sender

---
+++ backend/admin-backend/utils/mailSender.js
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer";
+
+async function mailSender(email, title, body){
+
+    try{
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            },
+            secure: false
+        });
+
+        const info = transporter.sendMail({
+            from: `Prarabdh ${process.env.MAIL_USER}`,
+            to: `${email}`,
+            subject: `${title}`,
+            html: `${body}`
+        });
+
+        console.log("Info:", info);
+
+        return info;
+    } catch(error){
+        return resizeBy.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error
+        });
+    };
+}
+
+export {
+    mailSender
+}
