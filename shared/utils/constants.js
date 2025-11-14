b4aa17d467066fe52b74afe4a2af45ab7ca4e882
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-14 17:36:51 +0530

    Added utils

---
+++ shared/utils/constants.js
@@ -0,0 +1,15 @@
+const ROLE_TYPE = {
+    CUSTOMER: "Customer",
+    ADMIN: "Admin",
+    SELLER: "Seller"
+};
+
+const PRODUCT_STATUS = {
+    DRAFT: "Draft",
+    PUBLISHED: "Published"
+}
+
+export {
+    ROLE_TYPE,
+    PRODUCT_STATUS
+}
