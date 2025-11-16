4f956d545e2bc90fb5ea78213be48ecb2569e379
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-16 18:25:55 +0530

    Initial commit

---
+++ backend/seller-backend/index.js
@@ -0,0 +1,56 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { router as productRoutes } from "./src/routes/productRoutes";
+import { databaseConnect } from "../../shared/config/database";
+import dotenv from "dotenv";
+import { cloudinaryConnect } from "../../shared/config/cloudinary";
+import { success } from "zod";
+dotenv.config();
+
+const PORT = process.env.PORT || 4000
+
+const app = express();
+
+databaseConnect();
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175"
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/temp"
+}));
+
+cloudinaryConnect();
+
+app.use(express.json());
+
+app.use(urlencoded({
+    extended: true
+}));
+
+app.use(cookieParser());
+
+app.use("/api/v1/product", productRoutes);
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Welcome to the app!"
+    });
+});
+
+app.listen(()=> {
+    console.log(`App is running on port ${PORT}`);
+});
+
