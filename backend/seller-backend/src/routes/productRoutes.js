504f3c0a2752c8cb2624bc63994f4d5dcb33ea9f
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-16 19:13:25 +0530

    Added routes

--- backend/seller-backend/src/routes/productRoutes.js
+++ backend/seller-backend/src/routes/productRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { auth, isSeller } from "../../../../shared/middlewares/auth";
-import { addProductToCategory } from "../../../admin-backend/src/controllers/Category";
-import { createProduct, deleteProduct, editProducts, getSellerProducts } from "../controllers/Product";
+import { auth, isSeller } from "../../../../shared/middlewares/auth.js";
+import { addProductToCategory } from "../../../admin-backend/src/controllers/Category.js";
+import { createProduct, deleteProduct, editProducts, getSellerProducts } from "../controllers/Product.js";
 
-const router = Router()
+const router = Router();
 
 router.post("/addProductToCategory", auth, isSeller, addProductToCategory);
 router.post("/createProduct", auth, isSeller, createProduct);
@@ -13,4 +13,4 @@ router.delete("/deleteProducts", auth, isSeller, deleteProduct);
 
 export {
     router
-}
+};
