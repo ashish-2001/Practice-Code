2b323d89a7a73473659404dea3def1ad33e751ba
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-16 17:24:55 +0530

    Initial commit

---
+++ shared/index.js
@@ -0,0 +1,58 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { databaseConnect } from "./config/database";
+import { router as userRoutes} from "../shared/routes/userRoutes";
+import { router as contactRoutes } from "../shared/routes/contactRoutes";
+import dotenv from "dotenv";
+import { cloudinaryConnect } from "./config/cloudinary";
+
+
+dotenv.config();
+
+const app = express();
+
+databaseConnect();
+
+const PORT = process.env.PORT || 4000;
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175"
+]
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/temp"
+}));
+
+app.use(express.json());
+
+app.use(urlencoded({
+    extended: true
+}));
+
+app.use(cookieParser());
+
+cloudinaryConnect();
+
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/contact", contactRoutes);
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Welcome to the api!"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on  ${PORT}`);
+})
