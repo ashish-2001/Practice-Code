e3bf951e03165f602776c12a337c102ce857135e
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-17 17:27:38 +0530

    Added models

--- shared/models/Otp.js
+++ shared/models/Otp.js
@@ -1,6 +1,6 @@
 import mongoose, { Error } from "mongoose";
-import { otpTemplate } from "";
-import { mailSender } from "../utils/mailSender";
+import { otpTemplate } from "../mail/templates/emailVerificationTemplate.js";
+import { mailSender } from "../utils/mailSender.js";
 
 const otpSchema = new mongoose.Schema({
     otp: {
