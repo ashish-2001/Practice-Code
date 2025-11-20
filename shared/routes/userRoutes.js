76d191f7eab45a2d4d55a2ef31417f425532a398
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-20 14:59:31 +0530

    Added routes

--- shared/routes/userRoutes.js
+++ shared/routes/userRoutes.js
@@ -8,7 +8,7 @@ const router = Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.post("/sendOtp", sendOtp);
+router.post("/sendOtp", sendOtp)
 router.post("/resetPassword", resetPassword);
 router.put("changePassword", auth, changePassword);
 router.post("/resetPasswordToken", resetPasswordToken);
