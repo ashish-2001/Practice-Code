6ab35f9537c1b8253cf036ab62baaa42fa7db6b4
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-21 15:37:43 +0530

    Added cart model

---
+++ backend/models/Cart.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import z from "zod";
+
+const cartSchema = z.object({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        unique: true,
+        required: true
+    },
+
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: "Product"
+            },
+            quantity: {
+                type: Number,
+                min: 1
+            }
+        }
+    ]
+}, { timestamps: true });
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+export {
+    Cart
+}
