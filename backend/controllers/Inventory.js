f32ec9300fdd817ea9183afd4189236f8710b3fa
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-22 16:26:43 +0530

    Added inventory controllers

--- backend/controllers/Inventory.js
+++ backend/controllers/Inventory.js
@@ -1,98 +1,188 @@
-import mongoose from "mongoose";
 import z, { success } from "zod";
 import { Product } from "../models/Product";
 import { Inventory } from "../models/Inventory";
 import { User } from "../models/User";
 
-const inventoryValidator = new mongoose.Schema({
+const inventoryValidator = z.object({
+    product: z.string().min(1, "Product id is required!"),
     change: z.number().min(1, "Inventory is required!"),
     reason: z.enum(["Purchase", "Order Cancelled", "Stock update"]),
 })
 
-async function inventory(req, res){
+async function createInventory(req, res){
 
-    const parsedResult = inventoryValidator.safeParse(requestAnimationFrame.body);
+    try{
 
-    if(!parsedResult.success){
-        return res.status(403).json({
-            success: false,
-            message: "All the fields are required!"
-        });
-    };
+        const parsedResult = inventoryValidator.safeParse(req.body);
 
-    const {
-        change,
-        reason
-    }  = parsedResult.data;
-
-    const userId = req.user._id;
-    
-    const user = await User.findById(userId);
-
-    if(!user){
-        return res.status(404).json({
-            success: false,
-            message: "User not found!"
-        });
-    };
-
-    const productId = req.product?._id;
-
-    const productData = await Product.findById(productId).populate("category");
-
-    if(!productData){
-        return res.status(404).json({
-            success: false,
-            message: "Product not found!"
-        });
-    };
-    
-    if(req.user?.role === "Seller"){
-        if(String(productData.createdBy) !== String(req.user._id)){
+        if(parsedResult.success){
             return res.status(403).json({
                 success: false,
-                message: "You cannot updated another's product data!"
+                message: "All fields are required!"
             });
         };
-    };
 
-    if(reason === "Purchase"){
-        productData.productStock -= change
-    }
+        const {
+            product,
+            reason,
+            change
+        } = parsedResult.data;
 
-    if(reason === "Order Cancelled"){
-        productData.productStock += change
-    };
+        const userId = req.user._id;
 
-    if(reason === "Stock update"){
-        productData.productStock += change
-    }
+        const user = await User.findById(userId);
 
-    if(productData.productStock > 0){
-        return res.status(400).json({
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found!"
+            });
+        };
+
+        const productData = await Product.findById(product);
+
+        if(!productData){
+            return res.status(404).json({
+                success: false,
+                message: "Product not found!"
+            });
+        };
+
+        if(req.user.role === "Seller"){
+            if(String(productData.createdBy) !== String(req.user._id)){
+                return res.status(403).json({
+                    success: false,
+                    message: "User cannot update another's product data!"
+                });
+            };
+        };
+
+        if(reason === "Purchase"){
+            productData.productStock -= change;
+        }
+
+        if(reason === "Order cancelled"){
+            productData.productStock += change;
+        }
+
+        if(reason === "Stock update"){
+            productData.productStock += change;
+        }
+
+        if(productData.productStock < 0){
+            return res.status(403).json({
+                success: false,
+                message: "Product stock cannot be negative!"
+            });
+        };
+
+        const inventoryLog = await Inventory.create({
+            createdBy: userId,
+            product,
+            reason,
+            change
+        });
+
+        if(!inventoryLog){
+            return res.status(403).json({
+                success: false,
+                message: "Inventory not created!"
+            });
+        };
+
+        return res.status(200).json({
+            success: true,
+            message: "Inventory created successfully!",
+            inventory: inventoryLog,
+            product: productData
+        });
+    }catch(error){
+        return res.status(500).json({
             success: false,
-            message: "Stock cannot be negative!"
+            message: "Internal server error!",
+            error: error.message
         });
     };
-
-    await productData.save();
-
-    const inventoryLog = await Inventory.create({
-        product,
-        change,
-        reason
-    });
-
-    return res.status(200).json({
-        success: true,
-        message: "Inventory created successfully!",
-        productStock: productData.productStock,
-        inventory: inventoryLog
-    });
-
 };
+
+async function getAllInventory(req, res){
+
+    try{
+
+        let inventoryLogs;
+        if(req.user.role === "Admin"){
+
+            inventoryLogs = await Inventory.find()
+            .populate("createdBy", "firstName lastName email role")
+            .populate({
+                path: "product", 
+                select: "productName productStock category productPrice createdBy",
+                populate: [
+                    {
+                        path: "category",
+                        select: "categoryName"
+                    },
+                    {
+                        path: "createdBy",
+                        select: "firstName lastName email role"
+                    }
+                ]
+            });
+        }
+
+        else if(req.user.role === "Seller"){
+            const sellerProducts = await Product.find({
+                createdBy: req.user._id
+            }).select("_id");
+
+            const sellerProductIds = sellerProducts.map((p) => p._id);
+
+            inventoryLogs = await Inventory.find({
+                product:{
+                    $in: sellerProductIds
+                }
+            })
+            .populate("createdBy", "firstName lastName email role")
+            .populate({
+                path: "product", 
+                select: "productName productStock category productPrice createdBy",
+                populate: [
+                    {
+                        path: "category",
+                        select: "categoryName"
+                    },
+                    {
+                        path: "createdBy",
+                        select: "firstName lastName email role" 
+                    }
+                ]
+            });
+        }
+
+        else{
+            return res.status(402).json({
+                success: false,
+                message: "YOu are not allowed to access inventory logs!"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Inventory data fetched successfully",
+            count: inventoryLogs.length,
+            inventory: inventoryLogs
+        })
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error!",
+            error: error.message
+        });
+    }
+}
 
 
 export {
-    inventory
+    createInventory,
+    getAllInventory
 }
