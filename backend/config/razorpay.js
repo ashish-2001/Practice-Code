d40d03141eb0fa88cfd5f5feb0a975e275d2bc61
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-25 15:37:59 +0530

    Initial commit

---
+++ backend/config/razorpay.js
@@ -0,0 +1,12 @@
+import Razorpay from "razorpay";
+import dotenv from "dotenv";
+dotenv.config();
+
+let instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET
+});
+
+export {
+    instance
+}
