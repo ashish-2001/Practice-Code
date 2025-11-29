70844b9de0e782b91bacd71257a219d7c2256ce2
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-29 15:53:03 +0530

    Added order model

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -19,7 +19,8 @@ const orderSchema = new mongoose.Schema({
             quantity: {
                 type: Number,
                 required: true,
-                min: 1
+                min: 1,
+                default: 1
             },
 
             price: {
