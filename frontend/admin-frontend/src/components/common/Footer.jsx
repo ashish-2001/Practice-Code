144f4e58e16ca5d022964af1aa839abd61d5101b
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-30 19:16:03 +0530

    Initial commit

---
+++ frontend/admin-frontend/src/components/common/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHeart, FaGoogle, FaTwitter, FaYoutube } from "react-icons/fa";
+
+const BottomFooter = ["Privacy Policy", "Cookie Policy", "Terms"];
