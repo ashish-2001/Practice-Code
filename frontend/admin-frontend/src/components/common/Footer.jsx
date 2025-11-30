18d11d6d39ba255c3a5fcc2f1463f21e381da0a4
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-30 19:17:33 +0530

    Added Footer

--- frontend/admin-frontend/src/components/common/Footer.jsx
+++ frontend/admin-frontend/src/components/common/Footer.jsx
@@ -3,3 +3,12 @@ import { Link } from "react-router-dom";
 import { FaHeart, FaGoogle, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const BottomFooter = ["Privacy Policy", "Cookie Policy", "Terms"];
+
+
+function Footer(){
+    
+}
+
+export {
+    Footer
+}
