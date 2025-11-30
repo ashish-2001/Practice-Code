0ae00f46c0b4d4f42a276ef23349c7800638ad9b
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-30 19:24:17 +0530

    Added navbar

---
+++ frontend/admin-frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react"
+import { HiSearch } from "react-icons/hi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { TiShoppingCart } from "react-icons/ti";
+import { Link, matchPath, useLocation, useNavigate } from "react-router-dom";
