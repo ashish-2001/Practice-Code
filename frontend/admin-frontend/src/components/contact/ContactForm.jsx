5415c3a3a3f9ee7b042ffffafd68115a3a406147
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-30 20:01:51 +0530

    Added contact form

--- frontend/admin-frontend/src/components/contact/ContactForm.jsx
+++ frontend/admin-frontend/src/components/contact/ContactForm.jsx
@@ -6,6 +6,46 @@ function ContactForm(){
 
     const [ loading, setLoading ] = useState(false);
 
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {
+            errors,
+            isSubmitSuccessful
+        }} = useForm();
+
+        useEffect(() => {
+            if(isSubmitSuccessful){
+                reset({
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    message: "",
+                    contactNumber: ""
+                })
+            }
+        }, [reset, isSubmitSuccessful]);
+
+        const onSubmit = async (data) => {
+            console.log(data);
+            try{
+                setLoading(true);
+                const contactNumber = data.countryCode+" "+data.contactNumber;
+                const countryCode = data.countryCode;
+                const payload = {
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    email: data.email,
+                    message: data.message,
+                    contactNumber,
+                    countryCode
+                }
+
+                const res = await 
+            }
+        }
+
     return(
         <div>
 
