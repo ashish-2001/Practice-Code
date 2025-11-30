94ddfed96bceb1f1d4f9eb794f88fcafd5b9fb00
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-30 19:41:30 +0530

    Added contact form

--- frontend/admin-frontend/src/components/contact/ContactForm.jsx
+++ frontend/admin-frontend/src/components/contact/ContactForm.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+function ContactForm(){
+
+    const [ loading, setLoading ] = useState(false);
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export { 
+    ContactForm
+}
