4c7b8876aa950c2d7cd567ddee9ee4fe2aae02b8
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-11-30 19:31:43 +0530

    Added rating stars

---
+++ frontend/admin-frontend/src/components/common/RatingStars.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { TiStarFullOutline, TiStarHalfOutline, TiStarOutline } from "react-icons/ti";
+
+function RatingStars(){
+
+}
+
+export {
+    RatingStars
+}
