dd99e10a358dfd852ff80c830be63ad33711b8c4
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-01 18:18:22 +0530

    Added product details api

--- frontend/admin-frontend/src/services/operations/productDetailsApi.jsx
+++ frontend/admin-frontend/src/services/operations/productDetailsApi.jsx
@@ -14,5 +14,35 @@ const {
     DELETE_PRODUCT_API,
     GET_FULL_PRODUCT_DETAILS_AUTHENTICATED,
     CREATE_RATING_API,
-    ADD_PRODUCT_TO_CATEGORY_API
+    ADD_PRODUCT_TO_CATEGORY_API,
+    SEARCH_PRODUCT_API
+} = productEndpoints;
+
+async function getAllProducts(){
+
+    const toastId = toast.loading("Loading...");
+    let result = [];
+
+    try{
+        const response = await apiConnector("GET", GET_ALL_PRODUCT_API)
+
+        console.log("Get all product api response:", response);
+
+        if(!response.data.success){
+            throw new Error(response.data.message);
+        }
+
+        result = response?.data?.data;
+
+    } catch(error){
+        console.log("Get all product data error", error);
+        toast.error(error.message);
+    }
+
+    toast.dismiss(toastId);
+    return result;
+}
+
+export {
+    getAllProducts
 }
