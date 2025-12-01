b802794a82c0dc0ee6ee127150ed6359e1528c1d
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-01 15:42:56 +0530

    Added auth api

---
+++ frontend/admin-frontend/src/services/operations/authApi.jsx
@@ -0,0 +1,49 @@
+import { setLoading } from "../../slices/authSlice";
+import { apiConnector } from "../apiConnector";
+import { endpoints } from "../apis";
+import toast from "react-hot-toast";
+
+const {
+    SEND_OTP_API,
+    SIGN_UP_API,
+    LOGIN_API,
+    RESET_PASS_TOKEN,
+    RESET_PASS_API
+} = endpoints;
+
+function sendOtp(email, navigate){
+
+    return async (dispatch) => {
+
+        const toastId = toast.loading("Loading...")
+        dispatch(setLoading(true));
+
+        try{
+            const response = await apiConnector("POST", SEND_OTP_API, {
+                email,
+                checkUserPresent: true
+            });
+
+            console.log("Send otp api response", response.data.success);
+
+            if(!response.data.success){
+                throw new Error(response.data.message);
+            }
+
+            toast.success("Otp sent successfully");
+            navigate("/verify-email")
+        } catch(error){
+            console.error("Send otp api error", error);
+            toast.error("Could not send otp");
+        }
+
+        dispatch(setLoading(false));
+        toast.dismiss(toastId);
+    } 
+}
+
+
+
+export {
+    sendOtp
+}
