99266de09351b2a8945b0f30c850ed5dd2b2a6c0
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-01 16:55:26 +0530

    Added auth slice

--- frontend/admin-frontend/src/slices/authSlice.jsx
+++ frontend/admin-frontend/src/slices/authSlice.jsx
@@ -3,7 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     signupData: null,
     loading: false,
-    token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null
+    token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null,
+    user: localStorage.getItem ("user") ? JSON.parse(localStorage.getItem("user")) : null
 };
 
 const authSlice = createSlice({
@@ -17,7 +18,11 @@ const authSlice = createSlice({
             state.loading = value.payload
         },
         setToken(state, value){
-            state.token = value.token
+            state.token = value.payload
+        },
+        setUser(state, value){
+            state.user = value.payload
+            localStorage.setItem("user", JSON.stringify(value.payload))
         }
     }
 });
