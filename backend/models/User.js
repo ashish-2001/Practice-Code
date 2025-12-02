3004a71a8a40cdc57c40cad7695822530f9b7223
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-02 17:47:40 +0530

    Added user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
 
     role: {
         type: String,
-        enum: ["Admin", "Seller", "Customer"],
+        enum: ["Admin", "Customer"],
         required: true
     },
 
