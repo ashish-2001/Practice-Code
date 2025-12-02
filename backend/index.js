01c534a05ff7f001285d68e4961a2f9a2d934f7d
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-02 17:27:16 +0530

    Added index

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,9 @@ const PORT = process.env.PORT || 4003;
 
 const allowedOrigins = [
     // "http://localhost:5173",
-    // "http://localhost:5174",
     // "http://localhost:5175"
 
     "http://admin-prarabdh.in",
-    "http://seller-prarabdh.in",
     "http://prarabdh.in"
 ]
 
