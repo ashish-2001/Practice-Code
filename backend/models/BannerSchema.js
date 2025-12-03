f34cc3bac8570897b17e45ea63c7898f7fa9e422
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-03 17:07:55 +0530

    Added banner model

---
+++ backend/models/BannerSchema.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const bannerSchema = new Schema({
+    title: {
+        type: String
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    link: {
+        type: String
+    },
+
+    startDate: {
+        type: Date,
+        default: null
+    },
+
+    endDate: {
+        type: Date,
+        default: null
+    },
+
+    priority: {
+        type: Number,
+        default: 10
+    },
+
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true });
+
+bannerSchema.index({ active: 1, startDate: 1, endDate: 1 });
+
+const Banner = mongoose.model("Banner", bannerSchema);
+
+export {
+    Banner
+}
