19b9290c2110b4f7b2b8a7dff0c6519d72f72397
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-03 17:55:52 +0530

    Added order item model

---
+++ backend/models/OrderItem.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { Schema } from "zod";
+
+const orderItemSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+
+    title: {
+        type: String
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    quantity: {
+        type: Number,
+        required: true
+    },
+
+    total: {
+        type: String,
+        required: true
+    },
+
+    attributes: Schema.Types.Mixed
+}, { _id: false });
+
+const OrderItem =  mongoose.model("OrderItem", orderItemSchema);
+
+export {
+    OrderItem
+}
