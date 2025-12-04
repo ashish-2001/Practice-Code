0c23ccaf90bd9ebd93ec5708495a838ee8f74b2a
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-04 16:33:45 +0530

    Added payment successful email template

---
+++ backend/mail/templates/paymentSuccessful.js
@@ -0,0 +1,95 @@
+function otpTemplate(otp){
+    
+    return `<!DOCTYPE html>
+    <html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>OTP Verification Email</title>
+        <style>
+            body {
+                background-color: #ffffff;
+                font-family: Arial, sans-serif;
+                font-size: 16px;
+                line-height: 1.4;
+                color: #333333;
+                margin: 0;
+                padding: 0;
+            }
+    
+            .container {
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                text-align: center;
+            }
+    
+            .logo {
+                max-width: 200px;
+                margin-bottom: 20px;
+            }
+    
+            .message {
+                font-size: 18px;
+                font-weight: bold;
+                margin-bottom: 20px;
+            }
+    
+            .body {
+                font-size: 16px;
+                margin-bottom: 20px;
+            }
+    
+            .cta {
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #FFD60A;
+                color: #000000;
+                text-decoration: none;
+                border-radius: 5px;
+                font-size: 16px;
+                font-weight: bold;
+                margin-top: 20px;
+            }
+    
+            .support {
+                font-size: 14px;
+                color: #999999;
+                margin-top: 20px;
+            }
+    
+            .highlight {
+                font-weight: bold;
+            }
+        </style>
+    
+    </head>
+    
+    <body>
+        <div class="container">
+            <a href="https://Prarabdh.in"><img class="logo"
+                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="Prarabdh Logo"></a>
+            <b class="message">Payment successful Email</b>
+            <div class="body">
+                <p>Dear User,</p>
+                <p>Your payment is successful for the product you purchased.</p> <br><br>
+                <p>Total paid: <b>${price}</b></p>
+                <p>Below is your receipt for the product you purchased</p>
+            </div>
+            <div class="support">
+                If you need assistance, please feel free to reach out to us at
+                <a href="mailto:info@Prarabdh.com">
+                    info@Prarabdh.com
+                </a>. 
+                We are here to help!
+            </div>
+            <p>Team Prarabdh</p>
+        </div>
+    </body>
+    
+    </html>`;
+} 
+
+export { 
+    otpTemplate
+}
