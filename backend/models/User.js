98a412d7a8be76d6694fc24d9b8fa874e651e48f
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-04 14:36:32 +0530

    Added user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -51,7 +51,7 @@ const userSchema = new mongoose.Schema({
             postalCode: String,
             country: {
                 type: String,
-                default: 'India'
+                default: 'Bharat'
             }
         }
     ],
