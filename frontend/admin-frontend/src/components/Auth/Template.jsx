7917a7a4a62dab6f83808be5e43d76c004ac350b
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-19 17:47:43 +0530

    Added template

---
+++ frontend/admin-frontend/src/components/Auth/Template.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import { SigninForm } from "./LoginForm";
+import { SignupForm } from "./SignupForm";
+
+function Template({ title, formType }){
+
+    const { loading } = useSelector((state) => state.auth);
+
+    return (
+        <div>
+            {
+                loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <div>
+                        <div>
+                            <h1>
+                                {title}
+                            </h1>
+                            {formType === "signup" ? <SignupForm/> : <SigninForm/>}
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export {
+    Template
+}
