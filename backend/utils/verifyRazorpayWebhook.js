40128fdfc4e93f9e1963c62cbeb5d6ac03db4c8f
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2025-12-25 18:09:06 +0530

    Added verify Razorpay Webhook

---
+++ backend/utils/verifyRazorpayWebhook.js
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+
+function verifyRazorpayWebhook(req){
+    const secret = process.env.RAZORPAY_WEBHOOK_SECRET;
+    const signature = req.headers["x-razorpay-signature"];
+
+    const expectedSignature = crypto.createHmac("sha256", secret)
+    .update(JSON.stringify(req.body)).digit("hex");
+
+    return expectedSignature === signature;
+}
+
+export {
+    verifyRazorpayWebhook
+}
