5426c472c7ab745c4fd609223ebb281758380da0
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-04 16:27:29 +0530

    Product slider

--- frontend/customer-frontend/src/components/catalog/Product_Slider.jsx
+++ frontend/customer-frontend/src/components/catalog/Product_Slider.jsx
@@ -1,12 +1,45 @@
 function Product_Slider({ products }){
 
-    return(
-        { products?.length ? (
-            
-        )}
+    return (
+        <>
+        {
+            products?.length ? (
+                <Swiper mousewheel={
+                    {
+                        enabled: true,
+                        forceToAxis: true
+                    }
+                }
+                keyboard={
+                    {
+                        enabled: true,
+                        onlyInViewport: true
+                    }
+                }
+                allowSlidePrev={true}
+                slidesPerView={1}
+                spaceBetween={20}
+                pagination={true}
+                loop={true}
+                modules={[Pagination, Navigation, FreeMode, MouseWheel, Keyboard]}
+                breakpoints={{
+                    300: { slidesPerView: 2.1, spaceBetween: 10},
+                    640: { slidesPerView: 2.2},
+                    1024: { slidesPerView: 3.1}
+                }}
+                className="max-h-[30rem]">
+                    {products?.map((course, index) => (
+                        <SwiperSlide key={index}>
+                            <Product_Card course={course} Height={"lg:h-[250px] h-[100px]"}/>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            ) : (
+                <div className="flex gap-4 overflow-hidden">
+                    
+                </div>
+            )
+        }
+        </>
     )
-}
-
-export {
-    Product_Slider
 }
