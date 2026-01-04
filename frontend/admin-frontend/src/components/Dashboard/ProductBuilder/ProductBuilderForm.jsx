0dc671aa5b6dcd305116a9c64a097e295b3de4aa
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-04 18:55:25 +0530

    Added product builder

---
+++ frontend/admin-frontend/src/components/Dashboard/ProductBuilder/ProductBuilderForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function ProductBuilderForm(){
+    const { token } = useSelector((state) => state.auth);
+
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+    const { product } = useSelector((state) => state.product);
+
+    function goNext(){
+        if(product.productContent.length > 0){
+            dispatch(setStep(3));
+        } else{
+            toast.error('Please add at least one product')
+        }
+    }
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = async (Data) => {
+        let result = null;
+        setLoading(true);
+        if(editSection)
+    }
+}
