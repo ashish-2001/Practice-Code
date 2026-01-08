4d80cc5cd96ee74e934ab30dcf834eb06b59d6ac
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-08 18:35:09 +0530

    Added navbar link

--- frontend/admin-frontend/src/components/common/data/Navbarlinks.jsx
+++ frontend/admin-frontend/src/components/common/data/Navbarlinks.jsx
@@ -1,6 +1,31 @@
 const NavbarLinks = [
     {
-
+        title: 'Best Seller',
+        path: '/best-seller'
+    }, 
+    {
+        title: "Men's",
+        path: '/men'
+    },
+    {
+        title: 'New',
+        path: '/new'
+    },
+    {
+        title: "Women's",
+        path: 'women'
+    },
+    {
+        title: 'Gift',
+        path: '/gift'
+    },
+    {
+        title: 'Collections',
+        path: '/collections'
+    }
+    {
+        title: 'About us',
+        path: '/about-us'
     }
 ];
 
