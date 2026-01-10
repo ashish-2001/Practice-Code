8ad2a1a19139f60758978fc95f9b90701687061e
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-10 16:27:20 +0530

    Added data

--- frontend/admin-frontend/src/components/common/data/Footerlink.jsx
+++ frontend/admin-frontend/src/components/common/data/Footerlink.jsx
@@ -87,9 +87,245 @@ const SocialMediaLinks = [
         logo: `<FaGoogle/>`,
         link: 'https://google.com'
     }
-]
+];
+
+const PopularSearchesLinks = [
+    {
+        title: 'For Women',
+        links: [
+            {
+                title: 'Demi-Fine Jewelry',
+                link: '/demi-fine-jewelry'
+            },
+            {
+                title: 'Rings for women',
+                link: '/rings-for-women'
+            },
+            {
+                title: 'Earnings For Women',
+                link: '/earings-for-women'
+            },
+            {
+                title: 'Bracelets For Women',
+                link: '/bracelets-for-women'
+            },
+            {
+                title: 'Pendants For Women',
+                link: '/pendants-for-women'
+            },
+            {
+                title: 'Necklaces For Women',
+                link: '/necklaces-for-women'
+            }
+        ]
+    },
+    {
+        title: 'For Men',
+        links: [
+            {
+                title: 'Rings For Men',
+                link: '/rings-for-men'
+            },
+            {
+                title: 'Pendants For Men',
+                link: '/pendant-for-men'
+            },
+            {
+                title: 'Chain For Men',
+                link: '/chain-for-men'
+            }
+        ]
+    },
+    {
+        title: 'Mangalsutra',
+        links: [
+            {
+                title: 'GOld Mangalsutra',
+                link: '/mangalsutra'
+            },
+            {
+                title: 'Diamond Mangalsutra',
+                link: '/diamond-mangalsutra'
+            },
+            {
+                title: 'Infinity Mangalsutra',
+                link: '/infinity-mangalsutra'
+            }
+        ]
+    },
+    {
+        title: 'Pendants',
+        links: [
+            {
+                title: 'Gold Pendants',
+                link: '/gold-pendants'
+            },
+            {
+                title: 'Diamond Pendants',
+                link: '/diamond-pendants'
+            },
+            {
+                title: 'Solitaire Pendants',
+                link: '/solitaire-pendants'
+            },
+            {
+                title: 'Evil Eye Pendants',
+                link: '/evil-eye-pendants'
+            }
+        ]
+    },
+    {
+        title: 'Bracelets',
+        links: [
+            {
+                title: 'Gold bracelets', 
+                link: '/gold-bracelets'
+            },
+            {
+                title: 'Diamond Bracelets',
+                link: '/diamond-bracelets'
+            },
+            {
+                title: 'Pearl Bracelets',
+                link: '/pearl-bracelets'
+            },
+            {
+                title: 'Evil Eye Bracelets',
+                link: '/chain-bracelets'
+            },
+            {
+                title: 'Stone Bracelets',
+                link: '/stone-bracelets'
+            },
+            {
+                title: 'Cuff Bracelets',
+                link: '/cuff-bracelets'
+            }
+        ]
+    },
+    {
+        title: 'Necklace',
+        links: [
+            {
+                title: 'Gold Necklace',
+                link: '/gold-necklace'
+            },
+            {
+                title: 'Diamond Necklace',
+                link: '/pearl-necklace'
+            },
+            {
+                title: 'Necklaces For Women',
+                link: '/necklace-for-women'
+            },
+            {
+                title: 'Evil Eye Necklace',
+                link: '/evil-eye-necklace'
+            },
+            {
+                title: 'Name Necklace',
+                link: '/name-necklace'
+            },
+            {
+                title: 'Emerald Necklace',
+                link: '/emerald-necklace'
+            }
+        ]
+    },
+    {
+        title: 'Rings',
+        links: [
+            {
+                title: 'Diamond Rings',
+                link: '/diamond-rings'
+            },
+            {
+                title: 'Gold Rings',
+                link: '/gold-rings'
+            },
+            {
+                title: 'Silver Rings',
+                link: '/silver-rings'
+            },
+            {
+                title: 'Solitaire Rings',
+                link: '/solitaire-rings'
+            },
+            {
+                title: 'Gemstone RIngs',
+                link: '/gemstone-rings'
+            },
+            {
+                title: 'Mens Rings',
+                link: '/mens-rings'
+            },
+            {
+                title: 'Engagement Ring',
+                link: 'engagement-rings'
+            },
+            {
+                title: 'Couple Rings',
+                link: '/couple-rings'
+            },
+            {
+                title: 'Wedding Rings',
+                link: '/wedding-rings'
+            },
+            {
+                title: 'Statement Ring',
+                link: '/statement-ring'
+            },
+            {
+                title: 'Emerald Rings',
+                link: '/emerald-rings'
+            }
+        ]
+    },
+    {
+        title: 'Earrings',
+        links: [
+            {
+                title: 'Gold Earrings',
+                link: '/gold-earrings'
+            },
+            {
+                title: 'Diamond Earrings',
+                link: '/diamond-earrings'
+            },
+            {
+                title: 'Solitaire Earrings',
+                link: '/solitaire-earrings'
+            },
+            {
+                title: 'Hoop Earrings',
+                link: '/hoop-earrings'
+            },
+            {
+                title: 'Stud Earrings',
+                link: '/stud-earrings'
+            },
+            {
+                title: 'Pearl Earrings',
+                link: 'pearl-earrings'
+            },
+            {
+                title: 'Emerald Earrings',
+                link: '/emerald-earrings'
+            },
+            {
+                title: 'Daily Wear Earrings',
+                link: '/daily-wear-earrings'
+            },
+            {
+                title: 'Dangler Earrings',
+                link: '/dangler-earrings'
+            }
+        ]
+    }
+];
 
 export {
     FooterLink,
-    SocialMediaLinks
+    SocialMediaLinks,
+    PopularSearchesLinks
 }
